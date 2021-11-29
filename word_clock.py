c6fa1b9bc283e2e043af091b2eb56049b5cee099
Steven Roh <hello@stevenroh.ch>
2021-11-29 11:43:19 +0000

    Progress

--- word_clock.py
+++ word_clock.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
+import json
 import datetime
 
 app = Flask(__name__)
@@ -28,8 +29,8 @@ WORDS_MINUTES = {
   45: 'quart',
   20: 'vingt',
   40: 'vingt',
-  25: 'vingt-cinq',
-  35: 'vingt-cinq',
+  25: 'vingt-cinq', # h+25
+  35: 'vingt-cinq', # h-25
   30: 'demie',
 }
 
@@ -43,7 +44,7 @@ def get_words():
   hours = now.hour
   minutes = now.minute - (now.minute % 5)
 
-  # 24h to 12h format
+  # 24h to 12h
   if hours > 12:
     hours -= 12
 
@@ -56,19 +57,18 @@ def get_words():
   
   if hours == 1:
     words.append('heure')
-  elif hours != 0 or hours != 12:
+  elif hours != 0 or hours != 12: # No "hours" text for midnight
     words.append('heures')
 
   # Display minus or plus or nothing
   if minutes == 0:
-    pass
+    pass # Display nothing
   elif minutes >= 35:
     words.append('moins')
     words.append(WORDS_MINUTES[minutes])
   else:
     if minutes == 15:
       words.append('et')
-  
     words.append(WORDS_MINUTES[minutes])
 
   return words
@@ -76,6 +76,36 @@ def get_words():
 
 def power_on_word(word):
   print(f'Power ON : {word}')
+
+def leds_for_word(word):
+  switcher = {
+    'il': [0, 1],
+    'est': [3, 4, 5],
+    'un': [-99],
+    'deux': [-99],
+    'trois': [-99],
+    'quatre': [-99],
+    'cinq': [-99],
+    'six': [-99],
+    'sept': [-99],
+    'huit': [-99],
+    'neuf': [-99],
+    'dix': [-99],
+    'onze': [-99],
+    'midi': [-99],
+    'minuit': [-99],
+    'cinq': [-99],
+    'dix': [-99],
+    'quart': [-99],
+    'vingt': [-99],
+    'vingt-cinq': [-99],
+    'demie': [-99],
+    'moins': [-99],
+    'et': [-99],
+    'heure': [-99],
+    'heures': [-99],
+  }
+  return switcher.get(word, "Invalid word")
 
 
 @app.route('/')
@@ -91,6 +121,22 @@ def hello():
 @app.route('/time')
 def time():
   return str(datetime.datetime.now())
+
+
+@app.route('/leds')
+def leds_status():
+  words = get_words()
+  leds_on = []
+
+  for word in words:
+    leds_on.extend(leds_for_word(word))
+
+  return jsonify(leds_on)
+
+@app.route('/clock')
+def clock():
+  return render_template('clock.html')
+
 
 if __name__ == '__main__':
   app.run(host='::', port=8000, debug=True)
