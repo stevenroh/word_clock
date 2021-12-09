4e03d71ace0f9c959ac3c21ecdb8b8e2749013ff
Steven Roh <hello@stevenroh.ch>
2021-12-09 16:31:06 +0000

    Refactor

--- iot.py
+++ iot.py
@@ -1,15 +1,16 @@
 import os
+from observer import Subscriber
 
 COMMANDS = {
-    'poweroff': 'shutdown -h now',
-    'reboot': 'reboot',
-    'test': 'echo test > /tmp/test.txt',
-    'on': '',
+  'poweroff': 'shutdown -h now',
+  'reboot': 'reboot',
+  'test': 'echo test > /tmp/test.txt',
 }
 
 INVALID_TASK = "Invalid task"
 
-def execute_if_valid(task):
+class IOTUtils(Subscriber):
+  def execute_if_valid(self, task):
     command = COMMANDS.get(task, INVALID_TASK)
 
     if command is not INVALID_TASK:
@@ -17,3 +18,6 @@ def execute_if_valid(task):
         return True
 
     return False
+
+  def update(self, message):
+    print("IOTUtils receive message : " + message)
