88f6e9972a5a9335fab9fb81783a72c654ae496c
Steven Roh <hello@stevenroh.ch>
2022-01-11 16:09:03 +0100

    Remove print

--- utils.py
+++ utils.py
@@ -5,8 +5,6 @@ LINES_TO_INVERT = [1, 3, 5, 7, 9]
 def get_fixed_led_id(identifier):
   current_line = identifier // MATRIX_WIDTH
 
-  print(str(identifier) + " " + str(current_line))
-
   if current_line in LINES_TO_INVERT:
     start_of_line = current_line * 11
     return start_of_line + (MATRIX_WIDTH - (identifier % start_of_line)) - 1
